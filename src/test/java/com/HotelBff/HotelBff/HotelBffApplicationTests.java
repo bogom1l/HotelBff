d16ac7462cb3faaf574fed173780f3a024fef32f
bogom1l <bogomil.stoev01@gmail.com>
2024-08-02 16:30:06 +0300

    Initial commit

---
+++ src/test/java/com/HotelBff/HotelBff/HotelBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelBff.HotelBff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
