6c660e063d3327d2b2f256b82ab259404f3bea52
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-06 11:51:25 +0300

    feign  -1 endpoint WORKS

--- domain/src/main/java/com/tinqinacademy/hotelbff/domain/HotelRestClient.java
+++ domain/src/main/java/com/tinqinacademy/hotelbff/domain/HotelRestClient.java
@@ -1,19 +1,11 @@
 package com.tinqinacademy.hotelbff.domain;
 
 
+import com.tinqinacademy.restexport.HotelRestExportClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-
+//todo @Headers("Content-Type: application/json")
 @FeignClient(name = "hotel", url = "http://localhost:8080")
-public interface HotelRestClient {
+public interface HotelRestClient extends HotelRestExportClient {
 
-    @GetMapping("/api/v1/hotel/rooms")
-    ResponseEntity<?> checkAvailableRooms(@RequestParam(required = false) LocalDate startDate,
-                                       @RequestParam(required = false) LocalDate endDate,
-                                       @RequestParam(required = false) String bedSize,
-                                       @RequestParam(required = false) String bathroomType);
 }
--- rest/src/main/java/com/tinqinacademy/hotelbff/rest/controllers/HotelController.java
+++ rest/src/main/java/com/tinqinacademy/hotelbff/rest/controllers/HotelController.java
@@ -1,17 +1,28 @@
 package com.tinqinacademy.hotelbff.rest.controllers;
 
 import com.tinqinacademy.hotelbff.api.restroutes.RestApiRoutes;
+import com.tinqinacademy.hotelbff.domain.HotelRestClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HotelController extends BaseController {
 
-    @GetMapping(RestApiRoutes.API_HOTEL)
-    public ResponseEntity<?> getHotel() {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    private final HotelRestClient hotelRestClient;
+
+    public HotelController(HotelRestClient hotelRestClient) {
+        this.hotelRestClient = hotelRestClient;
     }
+
+    @GetMapping("/room/{roomId}")
+    public ResponseEntity<?> getRoomBasicInfo(@PathVariable String roomId) {
+        return hotelRestClient.getRoomBasicInfo(roomId);
+    }
+
+
 
 }
