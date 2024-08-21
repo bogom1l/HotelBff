046bf450b2e50235d1d819aa8ef62015c3d820d3
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-21 12:09:56 +0300

    Update HotelBffException.java

--- api/src/main/java/com/tinqinacademy/hotelbff/api/exceptions/HotelBffException.java
+++ api/src/main/java/com/tinqinacademy/hotelbff/api/exceptions/HotelBffException.java
@@ -5,6 +5,11 @@ import org.springframework.http.HttpStatus;
 public class HotelBffException extends RuntimeException {
     private final HttpStatus status;
 
+    public HotelBffException(String message) {
+        super(message);
+        this.status = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
     public HotelBffException(String message, HttpStatus status) {
         super(message);
         this.status = status;
