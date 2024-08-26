15021a62f511c95364017634105d78c0cddd0931
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-26 10:29:13 +0300

    Added unbookRoom test

--- rest/src/test/java/com/tinqinacademy/hotelbff/rest/controllers/HotelControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/hotelbff/rest/controllers/HotelControllerTest.java
@@ -5,6 +5,8 @@ import com.tinqinacademy.authentication.api.operations.validatejwt.ValidateJwtOu
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomOutput;
 import com.tinqinacademy.hotel.api.operations.hotel.checkavailableroom.CheckAvailableRoomOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomOutput;
 import com.tinqinacademy.hotelbff.api.restroutes.RestApiRoutes;
 import com.tinqinacademy.hotelbff.domain.feignclients.AuthRestClient;
 import com.tinqinacademy.hotelbff.domain.feignclients.HotelRestClient;
@@ -28,8 +30,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -106,5 +107,38 @@ class HotelControllerTest {
                 .content(mapper.writeValueAsString(bookRoomInput))
         ).andExpect(status().isUnauthorized());
     }
+
+    @Test
+    void unbookRoomReturnsUnauthorized() throws Exception {
+        String jwtHeader = "Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJkMWFmYmI3My00OGJmLTQ5YTEtOWQ3NS0wZjc0OTBlYmVlMzYiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcyNDY1Njk4MiwiZXhwIjoxNzI0NjU3MjgyfQ.9H_zR-NBog0g3AMjCMHG59sroxjMj4mKAEpIVYQgZ4WpPGVfhDddqvyEhi5cNQcu";
+        String bookingId = UUID.randomUUID().toString();
+        String userId = "d1afbb73-48bf-49a1-9d75-0f7490ebee36";
+        String roomId = UUID.randomUUID().toString();
+
+        UnbookRoomInput unbookRoomInput = UnbookRoomInput.builder()
+                .userId(userId)
+                .build();
+
+        ValidateJwtOutput output = ValidateJwtOutput.builder().isValid(true).build();
+
+        when(authRestClient.validateJwt(jwtHeader))
+                .thenReturn(output);
+
+        Map<String, String> claims = new HashMap<>();
+        claims.put("sub", userId);
+        claims.put("role", "USER");
+        when(jwtDecoder.getPayloadFromJwt(jwtHeader))
+                .thenReturn(claims);
+
+        when(hotelRestClient.unbookRoom(roomId, unbookRoomInput))
+                .thenReturn(UnbookRoomOutput.builder().build());
+
+        mvc.perform(delete(RestApiRoutes.UNBOOK_ROOM, bookingId)
+                        .header(HttpHeaders.AUTHORIZATION, jwtHeader)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+
 
 }
