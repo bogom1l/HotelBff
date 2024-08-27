d59c026c6f2e87ebb13546d6e0fd32e0be931318
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-27 13:26:58 +0300

    Added test
    Error: failed to convert

--- rest/src/test/java/com/tinqinacademy/hotelbff/rest/controllers/CommentControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/hotelbff/rest/controllers/CommentControllerTest.java
@@ -1,79 +1,84 @@
-//package com.tinqinacademy.hotelbff.rest.controllers;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.tinqinacademy.authentication.api.operations.getallusers.GetAllUsersOutput;
-//import com.tinqinacademy.authentication.api.operations.validatejwt.ValidateJwtOutput;
-//import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
-//import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomOutput;
-//import com.tinqinacademy.hotel.api.operations.hotel.checkavailableroom.CheckAvailableRoomOutput;
-//import com.tinqinacademy.hotel.api.operations.hotel.getbookinghistory.GetBookingHistoryInput;
-//import com.tinqinacademy.hotel.api.operations.hotel.getbookinghistory.GetBookingHistoryOutput;
-//import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
-//import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomOutput;
-//import com.tinqinacademy.hotelbff.api.operations.comment.getcomments.GetCommentsBffOutput;
-//import com.tinqinacademy.hotelbff.api.restroutes.RestApiRoutes;
-//import com.tinqinacademy.hotelbff.domain.feignclients.AuthRestClient;
-//import com.tinqinacademy.hotelbff.domain.feignclients.HotelRestClient;
-//import com.tinqinacademy.hotelbff.rest.security.JwtDecoder;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.UUID;
-//
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-//public class CommentControllerTest {
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Autowired
-//    private ObjectMapper mapper;
-//
-//    @MockBean
-//    private HotelRestClient hotelRestClient;
-//
-//    @MockBean
-//    private AuthRestClient authRestClient;
-//
-//    @MockBean
-//    private JwtDecoder jwtDecoder;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//
-//    @Test
-//    void getAllComments() throws Exception {
-//        String roomId = UUID.randomUUID().toString();
-//        GetCommentsBffOutput output = GetCommentsBffOutput.builder()
-//                .comments(new ArrayList<>())
-//                .build();
-//        when(hotelRestClient.getComments(roomId)).thenReturn(output);
-//
-//        mvc.perform(get(RestApiRoutes.GET_ALL_COMMENTS, roomId))
-//                .andExpect(status().isOk());
-//    }
-//
-//
-//}
+package com.tinqinacademy.hotelbff.rest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tinqinacademy.authentication.api.operations.getallusers.GetAllUsersOutput;
+import com.tinqinacademy.authentication.api.operations.validatejwt.ValidateJwtOutput;
+import com.tinqinacademy.comments.api.operations.getcomments.GetCommentsOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
+import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.checkavailableroom.CheckAvailableRoomOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.getbookinghistory.GetBookingHistoryInput;
+import com.tinqinacademy.hotel.api.operations.hotel.getbookinghistory.GetBookingHistoryOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomOutput;
+import com.tinqinacademy.hotelbff.api.operations.comment.getcomments.GetCommentsBffOutput;
+import com.tinqinacademy.hotelbff.api.restroutes.RestApiRoutes;
+import com.tinqinacademy.hotelbff.domain.feignclients.AuthRestClient;
+import com.tinqinacademy.hotelbff.domain.feignclients.CommentsRestClient;
+import com.tinqinacademy.hotelbff.domain.feignclients.HotelRestClient;
+import com.tinqinacademy.hotelbff.rest.security.JwtDecoder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CommentControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private HotelRestClient hotelRestClient;
+
+    @MockBean
+    private AuthRestClient authRestClient;
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
+
+    @MockBean
+    private CommentsRestClient commentsRestClient;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllComments() throws Exception {
+        String roomId = UUID.randomUUID().toString();
+        GetCommentsBffOutput output = GetCommentsBffOutput.builder()
+                .comments(new ArrayList<>())
+                .build();
+
+        when(commentsRestClient.getAllComments(roomId)).thenReturn(GetCommentsOutput.builder().build());
+
+        mvc.perform(get(RestApiRoutes.GET_ALL_COMMENTS, roomId))
+                .andExpect(status().isOk());
+    }
+
+
+}
